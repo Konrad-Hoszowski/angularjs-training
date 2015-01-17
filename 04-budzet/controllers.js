e358f2c55050e79fa06095c84b15cd6e7316a340
Maciej Stopczyk <maciek.stopczyk@gmail.com>
2015-01-17 10:02:07 +0100

    my first repeat

--- 04-budzet/controllers.js
+++ 04-budzet/controllers.js
@@ -1,6 +1,25 @@
 var app = angular.module('budget', []);
 
 app.controller('MainCtrl', function ($scope) {
+    $scope.periods = ['2014-01', '2014-02', '2014-03'];
+    $scope.currentPeriod = $scope.periods[0];
 
+    $scope.expenses = [{
+            "title": "Czynsz",
+            "amount": 11,
+            "deadline": "2014-02-12",
+            "description": "Bo wylacza prad"
+        },
+        {
+            "title": "Czynsz2",
+            "amount": 12,
+            "deadline": "2014-02-12",
+            "description": "Bo wylacza prad"
+        },
+        {
+            "title": "Czynsz3",
+            "amount": 13,
+            "deadline": "2014-02-12",
+            "description": "Bo wylacza prad"
+        }];
 });
-
