8b775df0d50c463dff0376d16ad82a917b3d882d
Maciej Stopczyk <maciek.stopczyk@gmail.com>
2015-01-17 09:41:14 +0100

    add code snippets

--- 03-angular/controllers.js
+++ 03-angular/controllers.js
@@ -2,5 +2,29 @@ var app = angular.module('budget', []);
 
 app.controller('MainCtrl', function ($scope) {
 
+    $scope.sometext = "witam serdecznie";
+
+    $scope.users = [
+        {name: "Ala", gender: "female", description: "Ala desc"},
+        {name: "Olek", gender: "male", description: "Olek desc"}
+    ];
+    
+    
+    
+    
+     $scope.$watch('users', function(newValue, oldValue){
+        console.log("Old: ");
+        console.log(oldValue);
+        console.log("New:");
+        console.log(newValue);
+    }, true);
+
 });
 
+
+
+app.directive('mySuperCustomDirective', function(){
+    return {
+        template: 'Nie taki diabel straszny'
+    };
+});
